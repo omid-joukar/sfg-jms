8a9c43bd811c15ce39d15478074d87c911d38fa1
omid-joukar <joukaromid6@gmail.com>
2021-05-28 08:38:50 +0200

    init project.

---
+++ src/test/java/omid/springframework/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package omid.springframework.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
